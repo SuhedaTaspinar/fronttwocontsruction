0fd3705abb6b3da5a7c71208505781d0cc9b3fa9
Şüheda <92635912+SuhedaTaspinar@users.noreply.github.com>
2024-09-14 17:07:15 +0300

    Bug fixed

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -58,52 +58,52 @@ const Portfolio = ({ showHeading = true }: Props) => {
   const displayedServices = services.slice(-4);
 
   return (
-    <section className="portfolio_area area-padding" id="portfolio">
-      <div className="container">
-        {showHeading && (
-          <div className="area-heading">
-            <h3 className="line">Son Hizmetlerimiz</h3>
-          </div>
-        )}
+      <section className="portfolio_area area-padding" id="portfolio">
+        <div className="container">
+          {showHeading && (
+              <div className="area-heading">
+                <h3 className="line">Son Hizmetlerimiz</h3>
+              </div>
+          )}
 
-        <div className="filters portfolio-filter">
-          <ul>
-            <Link className="navbar-brand logo_h" href="/services">
-              <li className="active" data-filter="*">
-                Tümü
-              </li>
-            </Link>
-          </ul>
-        </div>
-        <div className="filters-content">
-          <div className="row portfolio-grid">
-            {displayedServices.length > 0 ? (
-              displayedServices.map((service) => (
-                <div className="col-lg-6 col-md-6 all following">
-                  <div className="single_portfolio">
-                    <Image width={555} height={419}
-                      className="img-fluid w-100"
-                      src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/uploads/${service.image}`}
-                      alt={service.description}
-                    />
-                    <div className="short_info">
-                      <p>{service.type}</p>
-                      <h4>
-                        {service.description}
-                      </h4>
-                    </div>
-                  </div>
-                </div>
-              ))
-            ) : (
-              <p style={{ textAlign: 'center', fontSize: '1.25rem', color: '#6b7280' }}>
-                Hizmet bulunamadı.
-              </p>
-            )}
+          <div className="filters portfolio-filter">
+            <ul>
+              <Link className="navbar-brand logo_h" href="/services">
+                <li className="active" data-filter="*">
+                  Tümü
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <div className="filters-content">
+            <div className="row portfolio-grid">
+              {displayedServices.length > 0 ? (
+                  displayedServices.map((service) => (
+                      <div key={service._id} className="col-lg-6 col-md-6 all following">
+                        <div className="single_portfolio">
+                          <Image width={555} height={419}
+                                 className="img-fluid w-100"
+                                 src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/uploads/${service.image}`}
+                                 alt={service.description}
+                          />
+                          <div className="short_info">
+                            <p>{service.type}</p>
+                            <h4>
+                              {service.description}
+                            </h4>
+                          </div>
+                        </div>
+                      </div>
+                  ))
+              ) : (
+                  <p style={{ textAlign: 'center', fontSize: '1.25rem', color: '#6b7280' }}>
+                    Hizmet bulunamadı.
+                  </p>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 }
 
